9bdd43bb4659abc47aa5f7bd7752de65e94e819a
Jan Friese <jfriese82@gmail.com>
2020-05-27 16:58:14 +0200

    change TouchableNativeFeedback to TouchableHighlight for ios comp.

--- components/ContactButtonComponent/ContactButton.js
+++ components/ContactButtonComponent/ContactButton.js
@@ -1,31 +1,40 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Text, View, TouchableNativeFeedback, Switch, AsyncStorage, Modal, TouchableOpacity } from 'react-native';
-import { Colors } from '../../constants/colors';
+import React, { useContext } from 'react';
+import { Text, View, TouchableHighlight } from 'react-native';
 import { ContactButtonStylesDark, ContactButtonStylesLight } from '../ContactButtonComponent/ContactButtonStyles';
 import { SettingsContext } from '../../Data/settingsContext';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import Constants from 'expo-constants';
 
-export default ContactButton = props => {
-
+export default ContactButton = (props) => {
     const { theme, push } = useContext(SettingsContext);
     const [currentTheme, setCurrentTheme] = theme;
     const [sendPushNotification, setSendPushNotification] = push;
 
     return (
-        <View style={currentTheme === 'light' ? ContactButtonStylesLight.settingRow : ContactButtonStylesDark.settingRow} >
-            <View style={currentTheme === 'light' ? ContactButtonStylesLight.modalContainer : ContactButtonStylesDark.modalContainer} >
-            </View>
-            <TouchableNativeFeedback onPress={() => props.setContactModal(true)}>
+        <View style={currentTheme === 'light' ? ContactButtonStylesLight.settingRow : ContactButtonStylesDark.settingRow}>
+            <View
+                style={currentTheme === 'light' ? ContactButtonStylesLight.modalContainer : ContactButtonStylesDark.modalContainer}
+            ></View>
+            <TouchableHighlight
+                style={{ flex: 1, justifyContent: 'center', flexDirection: 'row', width: '100%' }}
+                underlayColor='transparent'
+                onPress={() => props.setContactModal(true)}
+            >
                 <View style={currentTheme === 'light' ? ContactButtonStylesLight.wrapper : ContactButtonStylesDark.wrapper}>
                     <View style={currentTheme === 'light' ? ContactButtonStylesLight.leftContainer : ContactButtonStylesDark.leftContainer}>
-                        <Text style={currentTheme === 'light' ? ContactButtonStylesLight.titleText : ContactButtonStylesDark.titleText}>{props.title}</Text>
-                        <Text style={currentTheme === 'light' ? ContactButtonStylesLight.descriptionText : ContactButtonStylesDark.descriptionText}>{props.description}</Text>
+                        <Text style={currentTheme === 'light' ? ContactButtonStylesLight.titleText : ContactButtonStylesDark.titleText}>
+                            {props.title}
+                        </Text>
+                        <Text
+                            style={
+                                currentTheme === 'light'
+                                    ? ContactButtonStylesLight.descriptionText
+                                    : ContactButtonStylesDark.descriptionText
+                            }
+                        >
+                            {props.description}
+                        </Text>
                     </View>
                 </View>
-            </TouchableNativeFeedback>
+            </TouchableHighlight>
         </View>
-
     );
-
 };
