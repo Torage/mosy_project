c9272c6e0e1ce9bf149bbafc78d5af5aa43e569f
Klaus Bochmann <d.deviljho@gmail.com>
2020-07-05 13:12:19 +0200

    Listener für longPress auf HomeTab

--- navigation/HomeNavigator.js
+++ navigation/HomeNavigator.js
@@ -1,14 +1,29 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { HomeScreen } from '../screens/HomeScreen/HomeScreen';
 import { SettingsContext } from '../Data/settingsContext';
 import { Colors } from '../constants/colors';
 
+import { useNavigation } from '@react-navigation/native';
+
 const HomeStack = createStackNavigator();
 
 export const HomeNavigator = () => {
+
+const navigation = useNavigation();
+
+useEffect(() => {
+    const unsubscribe = navigation.addListener('tabLongPress', e => {
+      console.log('go somewhere');
+      // navigation.navigate('ModalScreen')}
+    });
+  
+    return unsubscribe;
+  }, [navigation]);
+
     const { theme, push } = useContext(SettingsContext);
     const [currentTheme, setCurrentTheme] = theme;
+
     return (
         <HomeStack.Navigator
             headerMode='none'
